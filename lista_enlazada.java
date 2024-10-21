c1f3a9a420a8f67d9b83945dbac349c33273da0d
Gadiel-Sosa <gadielantoniososachan@gmail.com>
2024-10-21 10:57:43 -0600

    Add files via upload

---
+++ lista_enlazada.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList<Object> lista = new LinkedList<>();
+
+        lista.add("Palabra");
+        lista.add(5);
+        lista.add(17);
+        lista.add("palabra 2");
+
+        lista.removeLast();
+
+        System.out.println("El tamaño de la lista es: " + lista.size());
+        System.out.println(lista.getLast());  // Este método debe funcionar con LinkedList
+    }
+}
---
+++ lista_enlazada.py
@@ -0,0 +1,8 @@
+lista =[]
+lista.append("palabra 1")
+lista.append(5)
+lista.append(17)
+lista.append("palabra 2")
+lista.pop()
+print('tamaño de la lista: ', len(lista))
+print(lista[-1]) 
